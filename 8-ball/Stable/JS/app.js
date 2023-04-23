c7d2c0fa68a2e82dfefe38063bb03fb7c8c290ee
Nikan Ostovan <nikanostovan@gmail.com>
2023-04-22 19:31:19 -0700

    added multiple lang support

--- 8-ball/Stable/JS/app.js
+++ 8-ball/Stable/JS/app.js
@@ -1,37 +1,45 @@
 let questionInput;
-let responses = [
-    `It is certain`,
-    "Without a doubt",
-    "You may rely on it",
-    "Yes, definitely",
-    "It is decidedly so",
-    "As I see it, yes",
-    "Most likely",
-    `Yes`,
-    "Outlook good",
-    "Signs point to yes",
-    "Reply hazy try again",
-    "Better not tell you now",
-    `Ask again later`,
-    "Cannot predict now",
-    `Concentrate and ask again`,
-    "Don't count on it",
-    "Outlook not so good",
-    "My sources say no",
-    "Very doubtful",
-    "My reply is no"
-  ];
-  
+let responses;
+
+
   let button = document.getElementById("shake-button");
   let response = document.getElementById("response");
-  
-  function shakeBall() {
+  //console.log(responses);
+  async function shakeBall() {
+      let lang = document.getElementById("langMenu").value;
+      let responsePath;
+      switch (lang) {
+          case "english":
+              responsePath = 'Assets/English.json';
+              break;
+          case "chinese":
+              responsePath = "Assets/Chinese.json";
+              break;
+          default:
+              responsePath = 'Assets/English.json';
+              break;
+      }
+      console.log(responsePath);
+
+      await fetch(responsePath)
+          .then(response => response.json()) // Parse the JSON data
+          .then(data => {
+              console.log(data); // Log the parsed JSON data to the console
+
+              responses = data["responses"];
+              console.log(responses);
+          })
+          .catch(error => {
+              console.error('Error fetching JSON file:', error);
+          });
+
       response.innerHTML='';
       let index = Math.floor(Math.random() * responses.length);
       questionInput = document.getElementById("question").value;
       let nameInput = document.getElementById("name").value;
       let button = document.getElementById("shake-button");
       button.classList.add("shake");
+
       setTimeout(() => {
           button.classList.remove("shake");
           if (questionInput == '' || nameInput == '') {
