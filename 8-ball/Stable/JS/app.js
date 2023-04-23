83efb5ccd9ba894bb6b6139983f238b70e241c60
Nikan Ostovan <nikanostovan@gmail.com>
2023-04-22 20:35:08 -0700

    added comments

--- 8-ball/Stable/JS/app.js
+++ 8-ball/Stable/JS/app.js
@@ -1,13 +1,16 @@
-let questionInput;
-let responses;
+let responses;  //response typed by js
 
 
-  let button = document.getElementById("shake-button");
-  let response = document.getElementById("response");
-  //console.log(responses);
+  let button = document.getElementById("shake-button");//button element
+  let output = document.getElementById("response");//output element
+
+//collects user input and outputs the magic 8 ball answer
   async function shakeBall() {
-      let lang = document.getElementById("langMenu").value;
-      let responsePath;
+
+      let lang = document.getElementById("langMenu").value;//language selected by user
+      let responsePath; //path to the json file of the language selected
+
+      //switch statment to set the responsePath variable depending on the selected language
       switch (lang) {
           case "english":
               responsePath = 'Assets/English.json';
@@ -19,37 +22,45 @@ let responses;
               responsePath = 'Assets/English.json';
               break;
       }
-      console.log(responsePath);
 
+      //load in the json file selected
       await fetch(responsePath)
-          .then(response => response.json()) // Parse the JSON data
-          .then(data => {
-              console.log(data); // Log the parsed JSON data to the console
 
+          // Parse the JSON data
+          .then(response => response.json())
+          .then(data => {
               responses = data["responses"];
-              console.log(responses);
           })
           .catch(error => {
               console.error('Error fetching JSON file:', error);
           });
 
-      response.innerHTML='';
+      //reset the output tag
+      output.innerHTML='';
+
+      //get random number to use for the 8ball array
       let index = Math.floor(Math.random() * responses.length);
-      questionInput = document.getElementById("question").value;
+
+      //get the question and name input by user
+      let questionInput = document.getElementById("question").value;
       let nameInput = document.getElementById("name").value;
-      let button = document.getElementById("shake-button");
+
+      //add shake class so button does css animations
       button.classList.add("shake");
 
+      //timeout to show the output after the 8-ball animation is over
       setTimeout(() => {
+
+          //remove shake class for consecutive uses
           button.classList.remove("shake");
+
+          //check for empty answers
           if (questionInput == '' || nameInput == '') {
               alert("No input");
           } else {
-              response.textContent = `${responses[index]} ${nameInput}`;
+              output.textContent = `${responses[index]} ${nameInput}`;
           }
       }, 600);
-      console.log(questionInput);
-      console.log(nameInput);
   }
   
   button.addEventListener("click", shakeBall);
