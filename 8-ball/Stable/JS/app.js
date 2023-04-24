70a032521db1b40f97eaa67270c61466c4127430
Nikan Ostovan <nikanostovan@gmail.com>
2023-04-23 23:38:33 -0700

    added audio and lang prompts

--- 8-ball/Stable/JS/app.js
+++ 8-ball/Stable/JS/app.js
@@ -7,13 +7,7 @@ let selection = document.getElementById("langMenu");//language selection
 
 //collects user input and outputs the magic 8 ball answer
   async function shakeBall() {
-
-      //let lang = document.getElementById("langMenu").value;//language selected by user
-
-
-
-
-
+      let audio = document.getElementById("myAudio");
       //reset the output tag
       output.innerHTML='';
       output.classList.remove("mystical");
@@ -27,7 +21,7 @@ let selection = document.getElementById("langMenu");//language selection
 
       //add shake class so button does css animations
       button.classList.add("shake");
-
+      audio.play();
       //timeout to show the output after the 8-ball animation is over
       setTimeout(() => {
 
@@ -44,44 +38,46 @@ let selection = document.getElementById("langMenu");//language selection
       }, 600);
   }
 
-  /*  */
+  //Function sets the language of the app based on user input
 async function langMenuChange () {
-    //switch statment to set the responsePath variable depending on the selected language
-    console.log(selection.value);
-    let responsePath; //path to the json file of the language selected
-    let langLabel = document.getElementById("langLabel");
-    let header = document.getElementById("header");
-    let namePrompt = document.getElementById("namePrompt");
-    let qPrompt = document.getElementById("qPrompt");
 
+
+    let responsePath; //path to the json file of the language selected
+    let langLabel = document.getElementById("langLabel");//label for lang menu
+    let header = document.getElementById("header");//header element
+    let namePrompt = document.getElementById("namePrompt");//label for name input
+    let qPrompt = document.getElementById("qPrompt");//label for question input
+
+    //switch statment to set the responsePath variable depending on the selected language
+    //and the prompt names
     switch (selection.value) {
         case "spanish":
             responsePath='Assets/Spanish.json';
-            langLabel.innerHTML="lalalala";
-            namePrompt.innerHTML="lalalala";
-            qPrompt.innerHTML="lalalala";
-            header.innerHTML="lalalal";
+            langLabel.innerHTML="Elige el idioma del Magic 8-ball";
+            namePrompt.innerHTML="escribe su nombre";
+            qPrompt.innerHTML="escriba su pregunta";
+            header.innerHTML="Pregúntale al Magic 8-ball";
             break;
         case "english":
             responsePath = 'Assets/English.json';
-            langLabel.innerHTML="D";
-            namePrompt.innerHTML="d";
-            qPrompt.innerHTML="d";
-            header.innerHTML="l56";
+            langLabel.innerHTML="Choose 8-Ball Language:";
+            namePrompt.innerHTML="Enter Your Name:";
+            qPrompt.innerHTML="Enter Your Question:";
+            header.innerHTML="Ask the Magic 8-Ball";
             break;
         case "chinese":
             responsePath = "Assets/Chinese.json";
-            langLabel.innerHTML="c";
-            namePrompt.innerHTML="c";
-            qPrompt.innerHTML="c";
-            header.innerHTML="l2l";
+            langLabel.innerHTML="请选择Magic 8-ball的语言";
+            namePrompt.innerHTML="请输入你的名字";
+            qPrompt.innerHTML="请输入你的问题";
+            header.innerHTML="向Magic 8-ball提问";
             break;
         default:
             responsePath = 'Assets/English.json';
-            langLabel.innerHTML="defaultttttttt";
-            namePrompt.innerHTML="d";
-            qPrompt.innerHTML="d";
-            header.innerHTML="1111";
+            langLabel.innerHTML="Choose 8-Ball Language:";
+            namePrompt.innerHTML="Enter Your Name:";
+            qPrompt.innerHTML="Enter Your Question:";
+            header.innerHTML="Ask the Magic 8-Ball";
             break;
     }
 
@@ -97,7 +93,10 @@ async function langMenuChange () {
             console.error('Error fetching JSON file:', error);
         });
 }
+//initial loading of the json file
 langMenuChange();
+
+  //event listeners for click and lang menu state change.
   button.addEventListener("click", shakeBall);
   selection.addEventListener("change", langMenuChange);
   
